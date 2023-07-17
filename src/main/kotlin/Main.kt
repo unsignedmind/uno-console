236627d0852460c210b5f90d70f1ebddae608488
Niklas Schmidt <niklas.schmidt@kps.com>
2023-07-17 13:44:47 +0200

    simplified version

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,29 +10,23 @@ class UnoGame(private val numPlayers: Int) {
     private val deck = mutableListOf<UnoCard>()
     private val playerHands = mutableListOf<MutableList<UnoCard>>()
     private var currentPlayer = 0
-    private var clockwisePlayerTurns = true
     private var currentCard: UnoCard? = null
     private var gameOver = false
-    private var drawTwoCount = 0
-    private var drawTwoPlayed = false
     private var stack = mutableListOf<UnoCard>()
 
     init {
         val colors = arrayOf("Red", "Green", "Blue", "Yellow")
-        val values = arrayOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Skip", "Reverse", "Draw Two")
+        val values = arrayOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
 
-        // Create deck
+        // For each color create a card with every number
         for (color in colors) {
             for (value in values) {
                 deck.add(UnoCard(color, value))
-                if (value != "0") {
-                    deck.add(UnoCard(color, value))
-                }
             }
         }
         Collections.shuffle(deck)
 
-        // Deal cards to players
+        // For each player add seven cards to the players hand
         for (i in 0 until numPlayers) {
             val hand = mutableListOf<UnoCard>()
             for (j in 0 until 7) {
@@ -41,7 +35,7 @@ class UnoGame(private val numPlayers: Int) {
             playerHands.add(hand)
         }
 
-        // Set initial card
+        // Add a starter card to the stack of played cards
         val initialCard = deck.removeAt(0)
         stack.add(initialCard)
         currentCard = initialCard
@@ -63,36 +57,13 @@ class UnoGame(private val numPlayers: Int) {
         println("Top card: ${stack.last()}")
     }
 
-    private fun checkForDrawTwoCount(): Boolean {
-        if (drawTwoCount > 0 && !drawTwoPlayed) {
-            drawCards(drawTwoCount)
-            println("Player $currentPlayer draws $drawTwoCount cards due to accumulated Draw Two cards.")
-            drawTwoCount = 0
-            return true
-        }
-
-        return false
-    }
-
     private fun playCard(cardIndex: Int) {
         val card = getCurrentPlayerHand()[cardIndex]
-        if (card.color == currentCard?.color || card.value == currentCard?.value || card.value == "Wild") {
+        if (card.color == currentCard?.color || card.value == currentCard?.value) {
             getCurrentPlayerHand().removeAt(cardIndex)
             stack.add(card)
             println("Player $currentPlayer plays: $card")
             currentCard = card
-            if (card.value == "Reverse") {
-                clockwisePlayerTurns = !clockwisePlayerTurns
-            }
-            if (card.value == "Skip") {
-                checkForDrawTwoCount()
-                currentPlayer = getNextPlayerIndex()
-            }
-            if (card.value == "Draw Two") {
-                drawTwoCount += 2
-                drawTwoPlayed = true
-            }
-
         } else {
             println("Invalid card! Cannot play $card")
         }
@@ -100,37 +71,37 @@ class UnoGame(private val numPlayers: Int) {
 
     private fun hasValidCard(playerHand: List<UnoCard>): Boolean {
         for (card in playerHand) {
-            if (card.color == currentCard?.color || card.value == currentCard?.value || card.value == "Wild") {
+            if (card.color == currentCard?.color || card.value == currentCard?.value) {
                 return true
             }
         }
         return false
     }
 
+    /*
+    * Function sets the player count to 0 when the end of list of players is reached
+    * */
     private fun getNextPlayerIndex(): Int {
-        if(clockwisePlayerTurns) {
-            if(currentPlayer < (numPlayers -1)) {
-                return currentPlayer + 1
-            } else {
-                return 0
-            }
+        if(currentPlayer < (numPlayers -1)) {
+            return currentPlayer + 1
         } else {
-            if(currentPlayer > 0) {
-                return currentPlayer - 1
-            } else {
-                return numPlayers - 1
-            }
+            return 0
         }
     }
 
-    private fun drawCards(numCards: Int) {
-        repeat(numCards) {
+    /*
+    * Takes the used cards from the stack of played cards and puts them shuffled back to the deck
+    * */
+    private fun drawCards(amountOfCardsToDraw: Int) {
+        repeat(amountOfCardsToDraw) {
             if (deck.isEmpty()) {
                 deck.addAll(stack.subList(0, stack.size - 1))
                 Collections.shuffle(deck)
                 stack.clear()
                 stack.add(deck.removeAt(0))
             }
+
+            // player get gets a card
             playerHands[currentPlayer].add(deck.removeAt(0))
         }
     }
@@ -148,32 +119,29 @@ class UnoGame(private val numPlayers: Int) {
 
             if (!hasValidCard(getCurrentPlayerHand())) {
                 println("No valid card to play. Drawing a card...")
-                val drawnTwoCount = checkForDrawTwoCount()
-                if(drawnTwoCount) {
-                    drawCard()
-                }
+                drawCard()
                 println("Press Enter")
                 readln()
             } else {
-                var validInput = false
-                while (!validInput) {
+                var validUserInput = false
+                while (!validUserInput) {
                     println("Enter the index of the card you want to play:")
                     val input = readLine()?.toIntOrNull()
                     if (input != null && input in 0 until getCurrentPlayerHand().size) {
                         playCard(input)
-                        validInput = true
+                        validUserInput = true
                     } else {
                         println("Invalid card index!")
                     }
                 }
-                checkForDrawTwoCount() // todo player could play a skip card and avoid to draw the extra cards
+
+                // one could change this so that the remaining players can play on
                 if (getCurrentPlayerHand().isEmpty()) {
                     gameOver = true
                     println("Player $currentPlayer wins!")
                 }
             }
 
-            drawTwoPlayed = false
             currentPlayer = getNextPlayerIndex()
             println()
         }
